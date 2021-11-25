428087da37c569bafa9a0f70d2119ebfa9b36978
Krishna Janoliya <krishna.janola@gmail.com>
2021-11-25 21:33:24 +0530

    code committed

---
+++ DS-Algo/src/test/java/com/krishna/algorithm/DSAlgo/DsAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.algorithm.DSAlgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
