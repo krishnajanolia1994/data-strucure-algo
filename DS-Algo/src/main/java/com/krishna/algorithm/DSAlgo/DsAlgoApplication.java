eaa2c0865df8081e8fa058b22dfeafeb2a304ecf
Krishna Janoliya <krishna.janola@gmail.com>
2021-11-25 21:28:32 +0530

    code committted

---
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/DsAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.algorithm.DSAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAlgoApplication.class, args);
+	}
+
+}
