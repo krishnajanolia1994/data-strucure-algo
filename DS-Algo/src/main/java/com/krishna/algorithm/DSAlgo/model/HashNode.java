6da3341e0392f1e4f88d7b55af87d4a81a61e2d2
Krishna Janoliya <krishna.janola@gmail.com>
2021-11-25 22:23:00 +0530

    field added in hash node

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/model/HashNode.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/model/HashNode.java
@@ -1,6 +1,29 @@
 package com.krishna.algorithm.DSAlgo.model;
 
+
 public class HashNode {
 
+	private int key;
+	private int value;
+	private HashNode next;
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public HashNode getNext() {
+		return next;
+	}
+	public void setNext(HashNode next) {
+		this.next = next;
+	}
+	
   
 }
