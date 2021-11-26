0edfe13aeb6b4a7205d349ba12ba2559cc35fcd0
Krishna Janoliya <krishna.janola@gmail.com>
2021-11-26 23:38:55 +0530

    code committed adding adding new node

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/Controller/HashingController.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/Controller/HashingController.java
@@ -1,9 +1,23 @@
 package com.krishna.algorithm.DSAlgo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.krishna.algorithm.DSAlgo.constent.AppConstants;
+import com.krishna.algorithm.DSAlgo.model.HashNode;
+import com.krishna.algorithm.DSAlgo.service.HashingService;
 
 @RestController
 public class HashingController {
 
-  
+	@Autowired
+	private HashingService hashingService;
+	
+	@GetMapping(value  = AppConstants.HASHING + AppConstants.ADD+AppConstants.ELEMENT)
+	public HashNode[] addNode(@RequestParam("key") int key , @RequestParam("value") int value)
+	{
+		return hashingService.addNode(key, value);
+	}
 }
