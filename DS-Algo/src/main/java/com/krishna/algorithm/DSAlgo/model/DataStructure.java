d0f5d567f40baf08d033da73f109a81657676ebe
Krishna Janoliya <krishna.janola@gmail.com>
2021-12-05 17:32:23 +0530

    code committed  for adding linkrlist node

---
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/model/DataStructure.java
@@ -0,0 +1,53 @@
+package com.krishna.algorithm.DSAlgo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DataStructure {
+private List<String> list = new ArrayList<String>();
+	
+	private LinkedList head = null;
+	
+	private LinkedList linkedList = null;
+	
+	
+	private static DataStructure dataStrucure = null;
+	
+	private DataStructure() {}
+	
+	public static DataStructure getDataStruture() {
+		
+		if(dataStrucure == null) {
+			dataStrucure = new DataStructure();
+		}
+		return dataStrucure;
+		
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public LinkedList getLinkedList() {
+		return linkedList;
+	}
+
+	public void setLinkedList(LinkedList linkedList) {
+		this.linkedList = linkedList;
+	}
+
+	public LinkedList getHead() {
+		return head;
+	}
+
+	public void setHead(LinkedList head) {
+		this.head = head;
+	}
+	
+
+}
