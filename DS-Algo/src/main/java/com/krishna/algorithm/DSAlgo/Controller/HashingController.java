fbfdf118ee6d76edc2b3962940b860d0ffe82002
krishna janoliya <krishna.janolia@gmail.com>
2021-12-09 00:00:52 +0530

    HashingController

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/Controller/HashingController.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/Controller/HashingController.java
@@ -19,6 +19,5 @@ public class HashingController {
 	public HashNode[] addNode(@RequestParam("key") int key , @RequestParam("value") int value)
 	{
 		return hashingService.addNode(key, value);
-		
 	}
 }
