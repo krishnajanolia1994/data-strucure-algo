015ca17ddef031042fbadffc1eae185c6cf25d80
krishna janoliya <krishna.janolia@gmail.com>
2022-02-04 22:51:39 +0530

    added getRthRoot in AlgebraService

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/AlgebraService.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/AlgebraService.java
@@ -2,6 +2,6 @@ package com.krishna.algorithm.DSAlgo.service;
 
 public interface AlgebraService {
 	
-	public double getRthRoot(int root, int number);	
+	public double getRthRoot(int root, int number) ;	
 
 }
