e6567dfb83b7a2bb989a9d13b8fdefdad8f03054
krishna janoliya <krishna.janolia@gmail.com>
2022-02-04 22:41:38 +0530

    added addNode in HashingService

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/HashingService.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/HashingService.java
@@ -3,6 +3,6 @@ package com.krishna.algorithm.DSAlgo.service;
 import com.krishna.algorithm.DSAlgo.model.HashNode;
 
 public interface HashingService {
-	public HashNode [] addNode(int key, int value) ;	
+	public HashNode [] addNode(int key, int value);	
 
 }
