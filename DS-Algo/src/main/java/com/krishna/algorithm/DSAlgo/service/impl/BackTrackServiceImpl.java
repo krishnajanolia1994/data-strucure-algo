54332ce80d8e32b00899ca37aed32aa6cf31fd9a
krishna janoliya <krishna.janolia@gmail.com>
2022-02-05 12:04:57 +0530

    added printPermutation in BackTrackServiceImpl

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/impl/BackTrackServiceImpl.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/impl/BackTrackServiceImpl.java
@@ -32,7 +32,7 @@ public class BackTrackServiceImpl implements BackTrackService{
 			swap(str,i,0);
 			StringBuffer strToBePrint = new StringBuffer(str.charAt(0)+"");
 			StringBuffer tempb=printPermutation(allPermutation, new StringBuffer(str.substring(1, str.length())),length,strToBePrint1);
-			strToBePrint.append(tempb); 
+			strToBePrint.append(tempb);  
 			if(strToBePrint.length()==length)
 			{
 				System.out.println(strToBePrint);
