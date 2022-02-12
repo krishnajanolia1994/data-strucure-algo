b2f97a701d107e6fbb7bff5aed7f435407757c9d
krishna janoliya <krishna.janolia@gmail.com>
2022-02-12 12:15:41 +0530

    Update LinkedListServiceImpl.java

--- DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/impl/LinkedListServiceImpl.java
+++ DS-Algo/src/main/java/com/krishna/algorithm/DSAlgo/service/impl/LinkedListServiceImpl.java
@@ -14,7 +14,6 @@ public class LinkedListServiceImpl implements LinkedListService{
 	@Override
 	public LinkedList AddNode(String name, String surname) {
 		
-		
 		LinkedList linkedList = dataStructure.getHead();
 		
 		LinkedList linkedListNode = new LinkedList();
